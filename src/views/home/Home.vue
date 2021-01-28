41030771a1e5d8849bb0fdfbae09eab80246e66d
24732 <2473203182@qq.com>
2021-01-28 23:31:10 +0800

    2021.1.28的修改
    Signed-off-by: 24732 <2473203182@qq.com>

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,6 +5,7 @@
     <recommend-view :recommends="recommends"/>
     <feature-view></feature-view>
     <TabControl class="tab-control" :titles="['流行', '新款', '精选']"></TabControl>
+    <GoodsList :goods="goods['pop'].list"/>
     <ul>
       <li>列表</li>
       <li>列表</li>
@@ -59,8 +60,9 @@
 
   import NavBar from 'components/common/navbar/NavBar';
   import TabControl from "@/components/content/tabControl/TabControl";
+  import GoodsList from "@/components/content/goods/GoodsList";
 
-  import {getHomeMultidata} from "network/home";
+  import {getHomeMultidata, getHomeGoods} from "network/home";
 
   export default {
     name: "Home",
@@ -69,21 +71,42 @@
       RecommendView,
       FeatureView,
       NavBar,
-      TabControl
+      TabControl,
+      GoodsList
     },
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        goods: {
+          "pop": {page: 0, list: []},
+          "new": {page: 0, list: []},
+          "sell": {page: 0, list: []}
+        }
       }
     },
     created() {
       // 1.请求多个数据
-      getHomeMultidata().then(res => {
-        // this.result = res;
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
+      this.getHomeMultidata()
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods: {
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          // this.result = res;
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1
+        getHomeGoods(type, page).then(res => {
+            this.goods[type].list.push(...res.data.list)
+            this.goods[type].page += 1
+        })
+      }
     }
   }
 </script>
@@ -106,5 +129,6 @@
   .tab-control {
     position: sticky;
     top: 44px;
+    z-index: 9;
   }
 </style>
