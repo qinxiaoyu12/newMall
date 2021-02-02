72db9cc218eef425459b15cca00a95bf7ae8e333
24732 <2473203182@qq.com>
2021-02-02 22:33:28 +0800

    上拉加载

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,56 +1,19 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"/>
-    <recommend-view :recommends="recommends"/>
-    <feature-view></feature-view>
-    <TabControl class="tab-control" :titles="['流行', '新款', '精选']"
-                @tabClick="tabClick"></TabControl>
-    <GoodsList :goods="goodsShow"/>
-    <ul>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-    </ul>
+    <Scroll class="content"
+            ref="scroll"
+            :probe-type="3"
+            @scroll="contentScroll"
+            :pull-up-load="true" @pullingUp="loadMore">
+      <home-swiper :banners="banners"/>
+      <recommend-view :recommends="recommends"/>
+      <feature-view></feature-view>
+      <TabControl class="tab-control" :titles="['流行', '新款', '精选']"
+                  @tabClick="tabClick"></TabControl>
+      <GoodsList :goods="goodsShow"/>
+    </Scroll>
+    <BackTop @click.native="backClick" v-show="isShowBackTop"/>
   </div>
 </template>
 
@@ -62,6 +25,8 @@
   import NavBar from 'components/common/navbar/NavBar';
   import TabControl from "@/components/content/tabControl/TabControl";
   import GoodsList from "@/components/content/goods/GoodsList";
+  import Scroll from "@/components/common/scroll/Scroll";
+  import BackTop from "@/components/content/backTop/BackTop";
 
   import {getHomeMultidata, getHomeGoods} from "network/home";
 
@@ -73,7 +38,9 @@
       FeatureView,
       NavBar,
       TabControl,
-      GoodsList
+      GoodsList,
+      Scroll,
+      BackTop
     },
     data() {
       return {
@@ -84,7 +51,8 @@
           "new": {page: 0, list: []},
           "sell": {page: 0, list: []}
         },
-        currentType: 'pop'
+        currentType: 'pop',
+        isShowBackTop: true
       }
     },
     created() {
@@ -112,6 +80,8 @@
         getHomeGoods(type, page).then(res => {
             this.goods[type].list.push(...res.data.list)
             this.goods[type].page += 1
+
+            this.$refs.scroll.finishPullUp()
         })
       },
       tabClick(index) {
@@ -127,15 +97,28 @@
             this.currentType = 'sell'
             break
         }
+      },
+      backClick() {
+          this.$refs.scroll.scrollTo(0, 0)
+      },
+      contentScroll(position) {
+          this.isShowBackTop = (-position.y) > 1000
+      },
+      loadMore() {
+          this.getHomeGoods(this.currentType)
+
+          this.$refs.scroll.scroll.refresh()
       }
     }
   }
 </script>
 
 <style scoped>
-  #home {
-    padding-top: 44px;
-  }
+#home {
+  /*padding-top: 44px;*/
+  height: 100vh;
+  position: relative;
+}
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
@@ -151,5 +134,15 @@
     position: sticky;
     top: 44px;
     z-index: 9;
+  }
+
+  .content {
+    overflow: hidden;
+
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
   }
 </style>
