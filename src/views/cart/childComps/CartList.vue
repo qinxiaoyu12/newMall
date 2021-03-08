7b231a828a69cdf388795e6e18104da7f1aa2b25
tangyi <2473203182@qq.com>
2021-03-08 17:47:26 +0800

    发现Bug

---
+++ src/views/cart/childComps/CartList.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="cart-list">
+   <li v-for="item in cartList">{{item}}</li>
+ </div>
+</template>
+
+<script>
+  import {mapGetters} from 'vuex'
+    export default {
+        name: "CartList",
+        computed: {
+          ...mapGetters(['cartList'])
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
