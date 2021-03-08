e694739e7b7a760d65b81a8bf8eb426d94907983
tangyi <2473203182@qq.com>
2021-03-08 16:42:26 +0800

    购物车导航栏

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,13 +1,32 @@
 <template>
-  <h2>购物车</h2>
+  <div class="cart">
+    <nav-bar class="nav-bar">
+      <div slot="center" class="car-bar">购物车({{length}})</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from "../../components/common/navbar/NavBar";
+  import { mapGetters } from "vuex"
   export default {
-    name: "Cart"
+    name: "Cart",
+    components: {
+      NavBar
+    },
+    computed: {
+     ...mapGetters({
+       length: 'cartLength',
+       list: 'cartList'
+     })
+    }
   }
 </script>
 
 <style scoped>
-
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #ffffff;
+    font-weight: 700;
+  }
 </style>
