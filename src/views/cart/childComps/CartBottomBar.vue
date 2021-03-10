e5eaeced7c618b2e5a988ee1774913a56acdbc20
24732 <2473203182@qq.com>
2021-03-10 22:49:55 +0800

    发现bug

--- src/views/cart/childComps/CartBottomBar.vue
+++ src/views/cart/childComps/CartBottomBar.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="cart-bottom-bar">
-      <div>
-        <cart-bottom-bar/>
+      <div class="check-content">
+        <check-button class="check-button"/>
         <span>全选</span>
+      </div>
+
+      <div class="price">
+        合计: {{totalPrice}}
       </div>
     </div>
 </template>
@@ -13,6 +17,15 @@
         name: "CartBottomBar",
       components: {
         CheckButton
+      },
+      computed: {
+        totalPrice() {
+          return '￥' + this.$store.state.cartList.filter(item => {
+            return item.itemActive
+          }).reduce((preValue, item) => {
+              return preValue + item.price * item.count
+          }, 0).toFixed(2)
+        }
       }
     }
 </script>
@@ -20,7 +33,26 @@
 <style scoped>
   .cart-bottom-bar {
     height: 40px;
-    background-color: red;
+    background-color: #eee;
     position: relative;
+    line-height: 40px;
+    display: flex;
+  }
+
+  .check-content {
+    display: flex;
+    align-items: center;
+    margin-left: 10px;
+  }
+
+  .check-button {
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
+    margin-right: 5px;
+  }
+
+  .price {
+    margin-left: 30px;
   }
 </style>
