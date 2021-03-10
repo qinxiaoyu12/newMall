5fefd719ada9f8f64397249916683ea14b0ee727
tangyi <2473203182@qq.com>
2021-03-10 17:42:40 +0800

    CartListBar

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -5,18 +5,22 @@
     </nav-bar>
 
     <cart-list></cart-list>
+
+    <cart-bottom-bar/>
   </div>
 </template>
 
 <script>
   import NavBar from "../../components/common/navbar/NavBar";
   import CartList from "./childComps/CartList";
+  import CartBottomBar from "./childComps/CartBottomBar";
   import { mapGetters } from "vuex"
   export default {
     name: "Cart",
     components: {
       NavBar,
-      CartList
+      CartList,
+      CartBottomBar
     },
     computed: {
      ...mapGetters({
