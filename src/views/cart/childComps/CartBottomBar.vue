3492d60dbf2dad1f9403e636d8385da2d0055f21
tangyi <2473203182@qq.com>
2021-03-11 10:35:01 +0800

    219

--- src/views/cart/childComps/CartBottomBar.vue
+++ src/views/cart/childComps/CartBottomBar.vue
@@ -8,6 +8,10 @@
       <div class="price">
         合计: {{totalPrice}}
       </div>
+
+      <div class="calculate">
+        去计算：({{checkLength}})
+      </div>
     </div>
 </template>
 
@@ -25,6 +29,9 @@
           }).reduce((preValue, item) => {
               return preValue + (item.price * item.count)
           }, 0).toFixed(2)
+        },
+        checkLength() {
+          return this.$store.state.cartList.filter(item => item.itemActive).length
         }
       }
     }
@@ -37,12 +44,15 @@
     position: relative;
     line-height: 40px;
     display: flex;
+
+    font-size: 14px;
   }
 
   .check-content {
     display: flex;
     align-items: center;
     margin-left: 10px;
+    width: 60px;
   }
 
   .check-button {
@@ -54,5 +64,13 @@
 
   .price {
     margin-left: 30px;
+    flex: 1;
+  }
+
+  .calculate {
+    width: 90px;
+    background-color: red;
+    color: #fff;
+    text-align: center;
   }
 </style>
