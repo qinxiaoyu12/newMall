c5ec209462095513e355b77ffc1d5dc6bb89149a
24732 <2473203182@qq.com>
2021-03-17 21:47:23 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from "@/store";
 
 import toast from 'components/common/toast'
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
@@ -11,6 +13,14 @@ Vue.prototype.$bus = new Vue()
 
 //安装toast插件
 Vue.use(toast)
+
+//解决300ms延迟问题
+FastClick.attach(document.body)
+
+//图片懒加载
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
